4bb05af6e7c03b85d1c7fe32c55ebe97ee59f781
gab-gil <gabriel.gilbert290@gmail.com>
2025-01-01 09:33:26 -0500

    feat: refactor angular rename feature

--- nvim/.config/nvim/lua/custom/angular.lua
+++ nvim/.config/nvim/lua/custom/angular.lua
@@ -63,8 +63,14 @@ function M.rename_component()
       end)
 
       inputs.input("Rename component", pascal_old_component, function(input)
+        print("PROBLEM")
         input = input:gsub("^%l", string.upper)
         local dashed_component_name = input:gsub("(%l)(%u)", "%1-%2"):lower()
+
+        local parent_folder_path, _ = utils.split_path(node.path)
+        local old_folder_path = node.path
+        local new_folder_path = parent_folder_path .. utils.path_separator .. dashed_component_name
+
         for _, sub_node in ipairs(state.tree:get_nodes(node:get_id())) do
           local hit = string.find(sub_node.name, (dashed_old_component .. ".component."))
           if hit ~= nil then
@@ -75,10 +81,6 @@ function M.rename_component()
             local oldUri = sub_node.path
 
             uv.fs_rename(oldUri, newUri, function(err)
-              if err ~= nil then
-                -- Show error
-              end
-
               -- INFO: Notify LSP
               -- clients is an array of Client[]. This means that ipairs will return the index as the key and client as the value
               for _, client in ipairs(clients) do
@@ -91,116 +93,133 @@ function M.rename_component()
                   },
                 })
               end
-            end)
 
-            local ts_hit = string.find(newUri, "%.component%.ts$")
-            if ts_hit ~= nil then
-              utils.open_file(state, newUri, "e")
+              local ts_hit = string.find(newUri, "%.component%.ts$")
+              if ts_hit ~= nil then
+                vim.schedule(function()
+                  utils.open_file(state, newUri, "e")
 
-              local bufnr = utils.find_buffer_by_name(newUri)
+                  local bufnr = utils.find_buffer_by_name(newUri)
 
-              local lnum, col = unpack(vim.fn.searchpos([[@Component\(.\|\n\)\+export class \zs\(\w\+\)\ze]], "n"))
+                  local lnum, col = unpack(vim.fn.searchpos([[@Component\(.\|\n\)\+export class \zs\(\w\+\)\ze]], "n"))
 
-              vim.api.nvim_set_current_buf(bufnr)
+                  vim.api.nvim_set_current_buf(bufnr)
 
-              -- TODO: Grab the selector value before the modification for a global search and replace using ripgrep + sed
+                  -- TODO: Grab the selector value before the modification for a global search and replace using ripgrep + sed
 
-              local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-              local content = table.concat(lines)
-              local selector = content:match([[
-                selector.-['|"](.-)['|"]
-              ]])
+                  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+                  local content = table.concat(lines)
 
-              print("CONTENT: " .. content)
+                  local regex = "selector.-['|\"](.-)['|\"]"
+                  local selector = content:match(regex)
 
-              local root_dir
+                  local root_dir
 
-              -- Loop through clients to find angularls
-              for _, client in ipairs(clients) do
-                if client.name == "angularls" then
-                  root_dir = client.config.root_dir
-                end
-              end
-
-              -- vim.fn.system(
-              --   "cd "
-              --     .. root_dir
-              --     .. "&& rg '"
-              --     .. selector
-              --     .. "' -l | xargs sed -i 's/<"
-              --     .. selector
-              --     .. ">/<"
-              --     .. dashed_component_name
-              --     .. ">/g'"
-              -- )
-              --
-              -- vim.fn.system(
-              --   "cd "
-              --     .. root_dir
-              --     .. "&& rg '"
-              --     .. selector
-              --     .. "' -l | xargs sed -i 's/</"
-              --     .. selector
-              --     .. ">/</"
-              --     .. dashed_component_name
-              --     .. ">/g'"
-              -- )
-
-              vim.api.nvim_command([[:%s/selector.\+\zs]] .. dashed_old_component .. [[\ze/]] .. dashed_component_name)
-              vim.api.nvim_command(
-                [[:%s/templateUrl[^']\+'\zs.\+\ze'/.\/]] .. dashed_component_name .. ".component.html"
-              )
-              vim.api.nvim_command([[:%s/styleUrl[^']\+'\zs.\+\ze'/.\/]] .. dashed_component_name .. ".component.less")
-
-              -- INFO: Notify LSP
-              local file_uri = vim.uri_from_fname(newUri)
-              for _, client in ipairs(clients) do
-                if client.name == "vtsls" then
-                  client.request("textDocument/rename", {
-                    newName = input .. "Component",
-                    textDocument = {
-                      uri = file_uri,
-                    },
-                    position = {
-                      line = lnum - 1,
-                      character = col - 1,
-                    },
-                  }, function(err, result, context, config)
-                    if err ~= nil then
-                      print(err)
-                    elseif result ~= nil then
-                      for uri, text_edits in pairs(result.changes) do
-                        local fname = vim.uri_to_fname(uri)
-                        local buf = vim.fn.bufadd(fname)
-
-                        vim.fn.bufload(buf)
-
-                        for _, edit in pairs(text_edits) do
-                          vim.api.nvim_buf_set_text(
-                            buf,
-                            edit.range.start.line,
-                            edit.range.start.character,
-                            edit.range["end"].line,
-                            edit.range["end"].character,
-                            { edit.newText }
-                          )
-                        end
-                        -- print(uri)
-                        -- print(dump(text_edit))
-                      end
-                    else
-                      print("NOTHING RETURNED")
+                  -- Loop through clients to find angularls
+                  for _, client in ipairs(clients) do
+                    if client.name == "angularls" then
+                      root_dir = client.config.root_dir
                     end
-                  end)
-                end
-              end
-            end
-          end
+                  end
 
-          -- DONE
-          -- TODO: Rename selector symbol in component.ts
-          -- TODO: Rename class name symbol  in component.ts
+                  vim.bo.ma = true
+
+                  vim.api.nvim_command(
+                    [[:%s/selector.\+\zs]] .. dashed_old_component .. [[\ze/]] .. dashed_component_name
+                  )
+                  vim.api.nvim_command(
+                    [[:%s/templateUrl[^']\+'\zs.\+\ze'/.\/]] .. dashed_component_name .. ".component.html"
+                  )
+                  vim.api.nvim_command(
+                    [[:%s/styleUrl[^']\+'\zs.\+\ze'/.\/]] .. dashed_component_name .. ".component.less"
+                  )
+
+                  vim.fn.system(
+                    "cd "
+                      .. root_dir
+                      .. "&& rg '"
+                      .. selector
+                      .. "' -l | xargs sed -i 's/<"
+                      .. selector
+                      .. ">/<"
+                      .. dashed_component_name
+                      .. ">/g'"
+                  )
+
+                  vim.fn.system(
+                    "cd "
+                      .. root_dir
+                      .. "&& rg '"
+                      .. selector
+                      .. "' -l | xargs sed -i 's/<\\/"
+                      .. selector
+                      .. ">/<\\/"
+                      .. dashed_component_name
+                      .. ">/g'"
+                  )
+
+                  vim.api.nvim_command("w!")
+
+                  -- -- INFO: Notify LSP
+                  local file_uri = vim.uri_from_fname(newUri)
+                  for _, client in ipairs(clients) do
+                    if client.name == "vtsls" then
+                      client.request("textDocument/rename", {
+                        newName = input .. "Component",
+                        textDocument = {
+                          uri = file_uri,
+                        },
+                        position = {
+                          line = lnum - 1,
+                          character = col - 1,
+                        },
+                      }, function(err, result, context, config)
+                        if err ~= nil then
+                          print(err)
+                        elseif result ~= nil then
+                          for uri, text_edits in pairs(result.changes) do
+                            local fname = vim.uri_to_fname(uri)
+                            local buf = vim.fn.bufadd(fname)
+
+                            vim.fn.bufload(buf)
+
+                            for _, edit in pairs(text_edits) do
+                              vim.api.nvim_buf_set_text(
+                                buf,
+                                edit.range.start.line,
+                                edit.range.start.character,
+                                edit.range["end"].line,
+                                edit.range["end"].character,
+                                { edit.newText }
+                              )
+                            end
+                          end
+                        end
+                      end)
+                    end
+                  end
+                end)
+              end
+            end)
+          end
         end
+        print("OLD: " .. old_folder_path)
+        print("NEW: " .. new_folder_path)
+        -- vim.schedule(function()
+        --   -- TODO: Do the folder renaming at the end
+        --   uv.fs_rename(node.path, new_folder_path, function()
+        --     for _, client in ipairs(clients) do
+        --       client.notify("workspace/didRenameFiles", {
+        --         files = {
+        --           {
+        --             oldUri = vim.uri_from_fname(node.path),
+        --             newUri = vim.uri_from_fname(new_folder_path),
+        --           },
+        --         },
+        --       })
+        --     end
+        --   end)
+        -- end)
       end)
       ::continue::
     end
