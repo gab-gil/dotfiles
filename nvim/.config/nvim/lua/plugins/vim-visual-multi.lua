56ad30fe0a38dee7eed3f1f1a8f8d802c35beeb0
gab-gil <gabriel.gilbert290@gmail.com>
2025-02-19 15:54:26 -0500

    vim-visual-multi-fix

--- nvim/.config/nvim/lua/plugins/vim-visual-multi.lua
+++ nvim/.config/nvim/lua/plugins/vim-visual-multi.lua
@@ -1,5 +1,16 @@
 return {
   {
     "mg979/vim-visual-multi",
+    init = function()
+      vim.g.VM_maps = {
+        ["Goto Next"] = "]v",
+        ["Goto Prev"] = "[v",
+        ["I CtrlB"] = "<M-b>",
+        ["I CtrlF"] = "<M-f>",
+        ["I Return"] = "<S-CR>",
+        ["I Down Arrow"] = "",
+        ["I Up Arrow"] = "",
+      }
+    end,
   },
 }
