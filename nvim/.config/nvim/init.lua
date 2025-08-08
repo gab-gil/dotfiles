989e0dcc1372eeae9631028cdaf457a80a789066
gab-gil <gabriel.gilbert290@gmail.com>
2025-08-08 07:43:20 -0400

    more visible line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FB508F", bold = true })
