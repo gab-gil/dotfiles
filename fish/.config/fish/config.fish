8283b8c3b0a44cb1bb61e770517a3a6a36b081ca
gab-gil <gabriel.gilbert290@gmail.com>
2025-12-21 12:53:12 -0500

    linux update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -46,3 +46,11 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
